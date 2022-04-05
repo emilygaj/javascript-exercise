608bee61f4cda20f22574fc9005cd6038ce70bdf
emilygaj <emilygaj@users.noreply.github.com>
2022-04-05 23:35:37 +0000

    deploy: 0d23074881115c4c8f0d065fdfc190cf17fd887f

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+var firstName = "Emily";
+var lastName = "Gajda";
+var age = 21;
+var favoriteFoodIs = "blueberry scones";
 
+console.log(
+  `My name is ${firstName}. I am ${age} years old and I love to eat ${favoriteFoodIs}.`
+);
+
+var num = 2;
+var result = num + num;
+console.log(result);
+var fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+}
